5bf66d0e3abd317eda3776a813a3d8bee53fa933
sophivlk <sophialoukili21@gmail.com>
2025-03-12 09:43:13 +0000

    main.cpp organisé

--- .github/main.cpp
+++ .github/main.cpp
@@ -1,61 +1,42 @@
 #include <iostream>
+#include <vector>
 #include <cstdlib>
 #include <ctime>
-
-// a rajouter apres? srand(time(NULL)) pour initialiser rand
 using namespace std;
 
-//---------------------------------------------------------------PLateau---------------------------------------------------------------
+//---------------------------------------------------------------Classe Attaque---------------------------------------------------------------
+class Attaque {
+protected:
+    int type; // 0 : Pierre, 1 : Feuille, 2 : Ciseaux
 
-//Taille du tableau
-const int Taille = 10;
-
-//Initialisation du tableau
-
-void initPlateau(vector<vector<Animal *>> &plateau) {
-    int TotalAnimal = (Taille * Taille) / 4; //25% du plateau est rempli
-
-    for (int i = 0; i < TotalAnimal / 2; i++){
-        Animal *Loup = new Loup(Taille, Taille);
-        Animal *Pierre = new Pierre(Taille, Taille);
-        plateau[Loup -> get(X)][Loup -> get(Y)] = Loup;
-        plateau[Pierre -> get(X)][Pierre -> get(Y)] = Pierre;
+public:
+    Attaque() {
+        type = rand() % 3; // Génère une attaque aléatoire
     }
-}
 
-void afficherPlateau(vector<vector<Animal *>> &plateau) {
-    //On parcourt  l'ensemble des cases
-    for (int i = 0; i < Taille; i++){
-        for (int j = 0; j < Taille; j++){
-            //Cas où la case est vide
-            if (plateau[i][j] == nullptr){
-                cout << ".  ";
-            }
-            //Comparaison du type de la case du plateau avec Loup : si le pointeur est un Loup alors la conversion est réussit (=true)
-            else if (dynamic_cast<Loup *> (plateau[i][j])){
-                cout << "L";
-            }
-            else{
-                cout << "P";
-            }
+    Attaque(int a) : type(a) {} // Crée une attaque spécifique
+
+    int getTypeAttaque() const { return type; }
+
+    bool resoudreAttaque(const Attaque &a) const {
+        // Vérifie si l'attaque actuelle gagne contre l'autre attaque
+        if ((type == 0 && a.type == 2) || (type == 1 && a.type == 0) || (type == 2 && a.type == 1)) {
+            return true;
         }
+        return false;
     }
-}
 
-int main() {
+    string getNomAttaque() const {
+        if (type == 0) return "Pierre";
+        else if (type == 1) return "Feuille";
+        else return "Ciseaux";
+    }
+};
 
-    vector<vector<Animal *>> plateau(Taille, vector<Animal *>(Taille,nullptr));
-
-    initPlateau(plateau);
-    afficherPlateau(plateau);
-
-    return 0;
-}
-
-//Classe Animal
+//---------------------------------------------------------------Classe Animal---------------------------------------------------------------
 class Animal {
 protected:
-    std::string nom;
+    string nom;
     int x, y;
     bool vivant;
     Attaque typeAttaque;
@@ -67,7 +48,9 @@ public:
         vivant = true;
     }
 
-    std::string getNom() const { return nom; }
+    virtual ~Animal() {} // Destructeur virtuel
+
+    string getNom() const { return nom; }
     int getX() const { return x; }
     int getY() const { return y; }
     bool getVivant() const { return vivant; }
@@ -78,46 +61,107 @@ public:
     virtual void setAttaque() = 0;
     virtual void deplace(int maxX, int maxY) = 0;
 
-    bool attaque(Animal& a) {
+    bool attaque(Animal &a) {
         if (typeAttaque.resoudreAttaque(a.getAttaque())) {
             a.setVivant(false);
             return true;
         }
         return false;
     }
+
+    virtual void afficher() const = 0;
 };
 
-
-//Classe Attaque
-class Attaque {
-    protected:
-    int type // 0 :pierre, 1: Feuille, 2:Ciseaux
-
-    public:
-    Attaque() {
-        type = rand() % 3; // Attaque aléatoire pierre feuille ou ciseaux
+//---------------------------------------------------------------Classes spécifiques---------------------------------------------------------------
+class Pierre : public Animal {
+public:
+    Pierre(int maxX, int maxY) : Animal(maxX, maxY) {
+        nom = "Pierre";
+        typeAttaque = Attaque(0); // Toujours "Pierre"
     }
 
-    Attaque(int a) : type(a) {}//crée une attaque spécifique
-    
-    int getTypeAttaque() const { return type; } // retourne l'attaque
-    
-    bool resoudreAttaque(Attaque &a) const { // envoie vrai si l’attaque sur laquelle est appliquée la fonction est "plus forte" que l’attaque passée en paramètre
-        if ( type == 0 && a.type == 2 || type == 1 && a.type == 0 || type == 2 && a.type == 1){
-            return True;
-        }
-        return false;
+    void setAttaque() override {} // Ne change jamais
+
+    void deplace(int maxX, int maxY) override {} // Ne se déplace pas
+
+    void afficher() const override { cout << " P "; }
+};
+
+class Loup : public Animal {
+public:
+    Loup(int maxX, int maxY) : Animal(maxX, maxY) {
+        nom = "Loup";
+        setAttaque();
     }
 
-    string getNomAttaque() const { // traduit le type numéro en string pour décrire les attaques
-        if (type == 0){
-            return pierre;
-        }
-        else if (type == 1){
-            return feuille;
-        }
-        else if (type == 2){
-            return ciseaux;
+    void setAttaque() override {
+        typeAttaque = Attaque(rand() % 3); // Aléatoire
+    }
+
+    void deplace(int maxX, int maxY) override {
+        x = rand() % maxX;
+        y = rand() % maxY;
+    }
+
+    void afficher() const override { cout << " L "; }
+};
+
+//---------------------------------------------------------------PLateau---------------------------------------------------------------
+const int Taille = 10;
+
+// Initialisation du plateau avec 25% des cases remplies
+void initPlateau(vector<vector<Animal *>> &plateau) {
+    int TotalAnimal = (Taille * Taille) / 4; // 25% du plateau
+    int animauxPlaces = 0;
+
+    srand(time(NULL)); // Initialisation du générateur aléatoire
+
+    while (animauxPlaces < TotalAnimal) {
+        int x = rand() % Taille;
+        int y = rand() % Taille;
+
+        if (plateau[x][y] == nullptr) { // Vérifie que la case est vide
+            if (animauxPlaces < TotalAnimal / 2)
+                plateau[x][y] = new Loup(Taille, Taille);
+            else
+                plateau[x][y] = new Pierre(Taille, Taille);
+
+            animauxPlaces++;
         }
     }
+}
+
+// Affichage du plateau
+void afficherPlateau(vector<vector<Animal *>> &plateau) {
+    for (int i = 0; i < Taille; i++) {
+        for (int j = 0; j < Taille; j++) {
+            if (plateau[i][j] == nullptr) {
+                cout << " . ";
+            } else {
+                plateau[i][j]->afficher();
+            }
+        }
+        cout << endl;
+    }
+}
+
+// Libération de la mémoire
+void libererPlateau(vector<vector<Animal *>> &plateau) {
+    for (int i = 0; i < Taille; i++) {
+        for (int j = 0; j < Taille; j++) {
+            delete plateau[i][j]; // Libère la mémoire allouée
+            plateau[i][j] = nullptr;
+        }
+    }
+}
+
+//---------------------------------------------------------------Programme principal---------------------------------------------------------------
+int main() {
+    vector<vector<Animal *>> plateau(Taille, vector<Animal *>(Taille, nullptr));
+
+    initPlateau(plateau);
+    afficherPlateau(plateau);
+    libererPlateau(plateau);
+
+    return 0;
 }
