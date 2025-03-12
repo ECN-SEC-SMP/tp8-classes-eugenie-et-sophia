5acee83f831d80834a559cf0dcf32f5a8f575be9
sophivlk <sophialoukili21@gmail.com>
2025-03-12 08:41:09 +0100

    classe Animal

--- .github/main.cpp
+++ .github/main.cpp
@@ -1,2 +1,41 @@
 #include <iostream>
-#include stdlib.h
+#include <cstdlib>
+#include <ctime>
+
+//Classe Animal
+class Animal {
+protected:
+    std::string nom;
+    int x, y;
+    bool vivant;
+    Attaque typeAttaque;
+
+public:
+    Animal(int maxX, int maxY) {
+        x = rand() % maxX;
+        y = rand() % maxY;
+        vivant = true;
+    }
+
+    std::string getNom() const { return nom; }
+    int getX() const { return x; }
+    int getY() const { return y; }
+    bool getVivant() const { return vivant; }
+    Attaque getAttaque() const { return typeAttaque; }
+
+    void setVivant(bool v) { vivant = v; }
+
+    virtual void setAttaque() = 0;
+    virtual void deplace(int maxX, int maxY) = 0;
+
+    bool attaque(Animal& a) {
+        if (typeAttaque.resoudreAttaque(a.getAttaque())) {
+            a.setVivant(false);
+            return true;
+        }
+        return false;
+    }
+};
+
+
+//Classe Attaque
